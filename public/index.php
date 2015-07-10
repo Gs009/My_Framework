4042b070cada47ca25557be95686c816a0f053e8
Gs009 <alhan.bosca@eptech.eu>
2015-07-10 15:36:00 +0200

    Public

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *   Index class file
+ *
+ *   Class Index
+ *
+ *
+ * PHP Version 5.4
+ *
+ * @category Nothing
+ * @package  Nothing
+ * @author   bosca_a <alhan.bosca@epitech.eu>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://localhost:8080/PHP_Avance_my_framework/
+ */
+require_once dirname(__FILE__) . "/../lib/MyCore/Core.php";
+
+MyCore\Core::run();
+
+?>
