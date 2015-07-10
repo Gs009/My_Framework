5da0cf6177084d68f9bfe9705f2e3f295352d2ae
Gs009 <alhan.bosca@eptech.eu>
2015-07-10 15:35:27 +0200

    Exception

---
+++ lib/MyCore/exceptions/NotFoundException.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *   Exceptions class file
+ *
+ *   Class Exceptions
+ *
+ *
+ * PHP Version 5.4
+ *
+ * @category Nothing
+ * @package  Nothing
+ * @author   bosca_a <alhan.bosca@epitech.eu>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://localhost:8080/PHP_Avance_my_framework/
+ */
+
+namespace MyCore\exceptions;
+
+use \Exception;
+/**
+ *   NotFoundException class
+ *
+ *   Class NotFoundException
+ *
+ * @category Nothing
+ * @package  Nothing
+ * @author   bosca_a <alhan.bosca@epitech.eu>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://localhost:8080/PHP_Avance_my_framework/
+ */
+class NotFoundException extends Exception
+{
+
+}
+
+?>
